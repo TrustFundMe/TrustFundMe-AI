86601f5059f91476af04a1f3f38d2a24d771f87e
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2026-03-19 13:05:03 +0700

    feat: update library for mobile

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
 app.use('/api', aiRoutes);
 
